d0eb7c908c7c64aae67de40a6e661ef024d369aa
saviola24 <saviolafenitr@gmail.com>
2026-03-31 05:10:15 +0300

    initial commit

---
+++ src/main/java/hei/td5spring/Td5SpringApplication.java
@@ -0,0 +1,13 @@
+package hei.td5spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Td5SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Td5SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hei/td5spring/Td5SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.td5spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Td5SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
